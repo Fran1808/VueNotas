a0797156539d2b679f0ded0c513c6b48e6045622
Fran1808 <francmaci01@gmail.com>
2022-07-19 13:12:37 -0500

    Create Principal.vue

---
+++ src/components/Principal.vue
@@ -0,0 +1,33 @@
+<template>
+<body style="background-color: #87ab69;">
+    <div class="row vh-100 justify-content-center align-items-center ">
+        <h3 class="text-center" style="background-color:bisque ; color: #87ab69; font-style: verdana;">
+            Post-Notes
+        </h3>
+        <div class="col-sm-4 ">
+          <div class="card text-center shadow-lg">
+            <div class="card-body text-success">
+              <h4 class="card-title">¿Nuevo por aqui?</h4>
+              <p class="card-text lead">Registrate para disfrutar de Post-Notes y ver nuestras promociones</p>
+              <a href="#" class="btn text-white " style="background-color: #87ab69;">Registrarme!</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-4 border-success">
+          <div class="card text-center shadow-lg">
+            <div class="card-body text-success">
+              <h4 class="card-title">Bienvenido de nuevo</h4>
+              <p class="card-text lead">¿Ya tienes una cuenta? Ingresa tus datos para continuar</p>
+              <a href="#" class="btn text-white" style="background-color: #87ab69;">Iniciar sesión</a>
+            </div>
+          </div>
+        </div>
+      </div>
+</body>
+
+</template>
+
+<style>
+
+
+</style>
